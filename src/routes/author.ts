2b84276acbc9c0130732a0185139a611c84efb1a
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-29 20:04:22 +0530

    add basic route to create new author

--- src/routes/author.ts
+++ src/routes/author.ts
@@ -29,4 +29,25 @@ app.get('/:id', (c) => {
   return c.json(author)
 })
 
+app.post('/', async (c) => {
+  const body: { name: string } = await c.req.json()
+
+  if (!body?.name) {
+    return c.json({ error: 'Name is required' }, 401)
+  }
+
+  if (body.name.length < 2) {
+    return c.json({ error: 'Name must be at least 2 characters long' }, 401)
+  }
+
+  const newAuthor = {
+    id: authors.length.toString(),
+    name: body.name,
+  }
+
+  authors.push(newAuthor)
+
+  return c.json(newAuthor, 201)
+})
+
 export default app
