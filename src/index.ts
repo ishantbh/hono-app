1bfec88fb52f6e819f2447269190a31457cff53f
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-02 19:42:38 +0530

    add dummy route for api-keys

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import { Hono } from 'hono'
 import { env } from './data/env.ts'
 import authRoutes from './routes/auth.ts'
 import authorRoutes from './routes/author.ts'
+import apiKeyRoutes from './routes/apiKey.ts'
 
 const app = new Hono()
 
 app.route('/authors', authorRoutes)
 app.route('/auth', authRoutes)
+app.route('/api-key', apiKeyRoutes)
 
 serve(
   {
