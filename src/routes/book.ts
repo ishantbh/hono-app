9bbd7c3ce65e7c99ec10b49a58f6d829f0021148
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-04 11:54:48 +0530

    add route to get all books with their authors

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -1,9 +1,13 @@
 import { Hono } from 'hono'
 
+import { db } from '../db/db.ts'
+
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.json({ message: 'Book Route' })
+app.get('/', async (c) => {
+  const books = await db.query.BookTable.findMany({ with: { author: true } })
+
+  return c.json(books)
 })
 
 export default app
