d4ef99648eeeead98314a1064a3c919d95744f20
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-04 11:49:32 +0530

    add dummy book route

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
 import { env } from './data/env.ts'
+import apiKeyRoutes from './routes/apiKey.ts'
 import authRoutes from './routes/auth.ts'
 import authorRoutes from './routes/author.ts'
-import apiKeyRoutes from './routes/apiKey.ts'
+import bookRoutes from './routes/book.ts'
 
 const app = new Hono()
 
-app.route('/authors', authorRoutes)
-app.route('/auth', authRoutes)
 app.route('/api-key', apiKeyRoutes)
+app.route('/auth', authRoutes)
+app.route('/authors', authorRoutes)
+app.route('/books', bookRoutes)
 
 serve(
   {
