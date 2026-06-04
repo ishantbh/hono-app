d600279abf2e3c6e447c7e45cecc0ba871c63dd3
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-04 11:58:02 +0530

    add route to get a book by id

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
+import { eq } from 'drizzle-orm'
 
 import { db } from '../db/db.ts'
+import { BookTable } from '../db/schema.ts'
 
 const app = new Hono()
 
@@ -8,6 +10,21 @@ app.get('/', async (c) => {
   const books = await db.query.BookTable.findMany({ with: { author: true } })
 
   return c.json(books)
+})
+
+app.get('/:id', async (c) => {
+  const { id } = c.req.param()
+
+  const book = await db.query.BookTable.findFirst({
+    where: eq(BookTable.id, id),
+    with: { author: true },
+  })
+
+  if (!book) {
+    return c.json({ error: 'Book not found' }, 404)
+  }
+
+  return c.json(book)
 })
 
 export default app
